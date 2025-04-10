00ecc81142619baa9cf2199f54473f945fb57dcb
Guilherme F. <40839511+Mavegui@users.noreply.github.com>
2025-04-10 13:49:17 -0300

    Update UserController.php
    Clean code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,6 @@ class UserController extends Controller
      */
     public function create()
     {
-        // Returns the view 'login.create' which contains the form to create a new user account.
         return view('login.create');  
     }
 
@@ -32,28 +31,18 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        // Validate the incoming request data for user creation.
-        // The validation rules and custom messages are provided in Portuguese.
+
         $request->validate([
             'firstName' => 'required',
             'lastName' => 'required',
             'email' => 'required|email',
             'password' => 'required|min:5|confirmed',
         ]);
-        
-        // Retrieve all input data from the request.
+
         $user = $request->all();
-        
-        // Encrypt the password before storing it in the database.
         $user['password'] = bcrypt($request->password);
-        
-        // Create a new user record in the database with the provided data.
         $user = User::create($user);
-        
-        // Log in the newly created user.
         Auth::login($user);
-        
-        // Redirect the user to the login page.
         return redirect()->route('login.index');
     }
 }
